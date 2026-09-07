f35a7c740999d879b5bd15dfd3371138cf085238
jura75 <jurabashmakov@gmail.com>
2026-09-07 15:23:55 +0300

    Update tabLinks URL in project-s-files-pak.js

--- project-s-files-pak.js
+++ project-s-files-pak.js
@@ -6,7 +6,7 @@
     }
 
     const tabLinks = {
-        1: "https://raw.githubusercontent.com/ВАШ_АККАУНТ/ВАШ_РЕПО/main/script1.js",
+        1: "https://raw.githubusercontent.com/jura75/project-s-files-pak/refs/heads/main/table%20units.js",
         2: "", 3: "", 4: "", 5: "", 6: "", 7: "", 8: "", 9: ""
     };
 
